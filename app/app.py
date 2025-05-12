48dfa8bba31cfeae3394ca716137909b428671dc
Ismail <boulahna.ismail@gmail.com>
2025-05-12 14:12:48 +0000

    init app.py

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from prometheus_client import start_http_server, Summary
+import time
+
+# Create a metric to track request processing time
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Start the Prometheus server to collect metrics
+start_http_server(8000)
+
+@app.route('/handle_request', methods=['POST'])
+@REQUEST_TIME.time()  # Track time taken by the request
+def handle_request():
+    data = request.json
+    query = data.get("query")
+    # Process the query
+    response = augment_response(query)
+    return jsonify({"response": response})
