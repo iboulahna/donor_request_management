ab22fb6215f80b2e2ed39b897ce22705c50603dd
Ismail <boulahna.ismail@gmail.com>
2025-05-14 12:06:34 +0000

    add flask migrate

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requ
 # Start the Prometheus server to collect metrics
 start_http_server(8000)
 
+
 @app.route('/handle_request', methods=['POST'])
 @REQUEST_TIME.time()  # Track time taken by the request
 def handle_request():
@@ -15,3 +16,19 @@ def handle_request():
     # Process the query
     response = augment_response(query)
     return jsonify({"response": response})
+
+from flask_migrate import Migrate
+from app.models.donor_model import db
+
+# pip install Flask-Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+# flask db init
+# flask db migrate -m "Create donor table"
+# flask db upgrade
