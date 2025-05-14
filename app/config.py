28b9a945e7ebdd0c3c471be611fe266a621ed006
Ismail <boulahna.ismail@gmail.com>
2025-05-14 12:02:01 +0000

    update

--- app/config.py
+++ app/config.py
@@ -1,6 +1,14 @@
 # In app/config.py
 
+# # For SQLAlchemy (PostgreSQL):
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/donor_db'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = 'your-secret-key'
+
+#  in Mongo db 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/donor_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGO_URI = 'mongodb://localhost:27017/donor_db'
     SECRET_KEY = 'your-secret-key'
